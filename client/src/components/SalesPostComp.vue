3f96f5ce79fd1ae3106aaf24652ad36b01b9df4c
stianjsu <stianjsu@stud.ntnu.no>
2022-02-14 14:55:42 +0100

    ✨ feat(client-ui): Added simple posts to homepage
    Added SalesPostComp.vue and integrated v-for and Vuex state for saving and showing posts on front page. Getter for getting post by index has been added to store
    
    Related to #9
    Co-authored-by: @sarakos, @ylvarf

---
+++ client/src/components/SalesPostComp.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="post">
+    <p>Username: {{ post.user }}</p>
+    <p>Event name: {{ post.eventName }}</p>
+    <p>Event Type: {{ post.eventType }}</p>
+    <p>Date: {{ new Date(post.dateAndTime).toISOString() }}</p>
+    <p>Price: {{ post.price }}kr</p>
+  </div>
+</template>
+
+<script>
+import { useStore } from "vuex";
+import { computed } from "vue";
+
+export default {
+  name: "SalesPostComp",
+  props: ["indexPost"],
+  setup(props) {
+    const store = useStore();
+
+    const post = computed(() => {
+      return store.getters.getPostByIndex(props.indexPost);
+    });
+
+    return { post };
+  },
+};
+</script>
+
+<style scoped>
+.post {
+  margin: 6px auto;
+  border: solid black 1px;
+  width: min(100%, 600px);
+}
+</style>
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,7 +1,21 @@
 import { createStore } from "vuex";
+import SalesPost from "../core/SalesPost";
 
 export default createStore({
-  state: {},
+  state: {
+    posts: [
+      new SalesPost("Stian", "Fest1", "superfest", Date.now(), 20),
+      new SalesPost("Sara", "Fest2", "superfest", Date.now(), 30),
+      new SalesPost("Ylva", "Fest3", "superfest", Date.now(), 40),
+      new SalesPost("Ylva", "Fest3", "superfest", Date.now(), 40),
+      new SalesPost("Ylva", "Fest3", "superfest", Date.now(), 40),
+    ],
+  },
+  getters: {
+    getPostByIndex: (state) => (index) => {
+      return state.posts[index];
+    },
+  },
   mutations: {},
   actions: {},
   modules: {},
