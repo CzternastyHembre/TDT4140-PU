12fe2ea42ee8ca42cff25612a9d005fa253c3b18
stianjsu <stianjsu@stud.ntnu.no>
2022-02-14 23:46:36 +0100

    ✨ feat(client-ui): Header navigation
    Added header component to app, fixed to the top. Cleans up routing in the app
    
    Related to #4, #10

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,22 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div class="appWrapper">
+    <div id="nav">
+      <header-nav />
+    </div>
+    <div class="appContent">
+      <router-view />
+    </div>
   </div>
-  <router-view />
 </template>
+
+<script>
+import HeaderNav from "../src/components/HeaderNav.vue";
+export default {
+  components: {
+    HeaderNav,
+  },
+};
+</script>
 
 <style>
 #app {
@@ -16,7 +28,20 @@
 }
 
 #nav {
-  padding: 30px;
+  position: fixed; /* fixing the position takes it out of html flow - knows
+                   nothing about where to locate itself except by browser
+                   coordinates */
+  left: 0; /* top left corner should start at leftmost spot */
+  top: 0; /* top left corner should start at topmost spot */
+  width: 100%; /* take up the full browser width */
+  z-index: 200; /* high z index so other content scrolls underneath */
+  height: 70px; /* define height for content */
+  box-shadow: 0 4px 6px -1px rgba(0, 0, 0, 0.1),
+    0 2px 4px -1px rgba(0, 0, 0, 0.06);
+  background: #fff;
+}
+.appContent {
+  padding-top: 70px;
 }
 
 #nav a {
---
+++ client/src/components/HeaderNav.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="footerBox">
+    <div></div>
+    <div class="navButtons">
+      <router-link to="/">Home</router-link>
+      <router-link to="/about">About</router-link>
+    </div>
+    <div class="login">Login</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderNav",
+};
+</script>
+
+<style scoped>
+.footerBox {
+  margin: auto;
+  width: 80%;
+  padding: auto;
+  display: grid;
+  grid-template-rows: 1fr;
+  grid-template-columns: 1fr 3fr 1fr;
+}
+
+.footerBox div {
+  padding-top: 23px;
+}
+
+.navButtons a {
+  padding-left: 20px;
+  padding-right: 20px;
+}
+</style>
