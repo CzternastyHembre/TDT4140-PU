6a7d64f60affe26d0683d31e767fc4a6dc17ec38
mattisch <mattich@stud.ntnu.no>
2022-02-21 08:56:46 +0100

    ✨ feat(client-ui): add hash in User.js
    Hashing the password in User.js
    
    Closes: #3

--- client/src/core/Hashing.js
+++ client/src/core/Hashing.js
@@ -1,4 +1,4 @@
 const hash = (data) => {
-    return require("crypto").createHash('sha256').update(data).digest('hex')
-}
+  return require("crypto").createHash("sha256").update(data).digest("hex");
+};
 module.exports = hash;
--- client/src/core/User.js
+++ client/src/core/User.js
@@ -1,3 +1,4 @@
+const hash = require("./Hashing");
 export default class User {
   //TODO Profile picture
   /**
@@ -94,7 +95,7 @@ export default class User {
    */
   setPassword(_password) {
     this.validPassword(_password);
-    this.password = _password;
+    this.password = hash(_password);
   }
 
   /**
