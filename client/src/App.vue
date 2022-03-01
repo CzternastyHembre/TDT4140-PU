b5e0df6278dc5500fff647448ee199a5477cb552
ylvarf <ylvarf@stud.ntnu.no>
2022-03-01 13:07:56 +0100

    🌈 style(client): style commitbutton
    related to: #17

--- client/src/App.vue
+++ client/src/App.vue
@@ -84,11 +84,28 @@ body {
   flex-direction: column;
 }
 
-.basicForm input,
-.submitButton {
+.basicForm input {
   font-size: 1em;
   display: inline-block;
   size: 1em;
   width: 80%;
 }
+
+.submitButton {
+  padding: 10px 20px;
+  width: 35%;
+  font-size: 17px;
+  border-radius: 20px;
+  margin: auto; 
+  border: 1px solid #6a706e;
+  box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14),
+    0 3px 1px -2px rgba(0, 0, 0, 0.12), 0 1px 5px 0 rgba(0, 0, 0, 0.2)
+
+}
+
+.submitButton:hover {
+  box-shadow: 0 2px 6px black;
+}
+
+
 </style>
