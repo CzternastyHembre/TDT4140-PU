38f96ffd663640cc6f23a7fcaf8eade0141cf31f
stianjsu <stianjsu@stud.ntnu.no>
2022-03-06 01:21:02 +0100

    🐳 chore(root, client-core): npm run full app
    Fixed name on command used for running both ui and server in same terminal. Added route guards in vue-router to redirect not logged in users to login page where appropriate

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import store from "../store/index";
 import Home from "../views/Home.vue";
 import SignUp from "../views/SignUp.vue";
 import LogIn from "../views/LogIn.vue";
@@ -10,33 +11,58 @@ const routes = [
     path: "/",
     name: "Home",
     component: Home,
+    meta: {
+      requireLogIn: false,
+    },
   },
   {
     path: "/login",
     name: "LogIn",
     component: LogIn,
+    meta: {
+      requireLogIn: false,
+    },
   },
 
   {
     path: "/signup",
     name: "SignUp",
     component: SignUp,
+    meta: {
+      requireLogIn: false,
+    },
   },
   {
     path: "/createpost",
     name: "CreatePost",
     component: CreatePost,
+    meta: {
+      requireLogIn: true,
+    },
   },
   {
     path: "/UserProfile",
     name: "UserProfile",
     component: UserProfile,
+    meta: {
+      requireLogIn: true,
+    },
   },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.matched.some((res) => res.meta.requireLogIn)) {
+    if (!store.state.activeUser) {
+      next({ name: "LogIn" });
+      return;
+    }
+  }
+  next();
 });
 
 export default router;
