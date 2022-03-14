3b3b8155797aa33c0f61612fdf6e4e5933861185
Vetlest <Vetlest@stud.ntnu.no>
2022-03-14 09:07:43 +0100

    Co-authored-by: Hans Gunleik Simones <hansgs@ntnu.no>

--- client/src/components/UserProfileView.vue
+++ client/src/components/UserProfileView.vue
@@ -2,9 +2,8 @@
   <div class="profilePost">
     <div class="profileHeader">
       <h2>{{ activeUser.firstName }} {{ activeUser.lastName }}</h2>
-      <img src="image" />
     </div>
-
+    <img class="pic" :src="StianSulebak" />
     <div class="elementCC">
       <div class="elementContainer">
         <div>Email</div>
@@ -35,23 +34,34 @@
 </template>
 
 <script>
+import StianSulebak from "@/assets/StianSulebak.png";
+
 export default {
   name: "UserProfileView",
   props: {
     activeUser: Object,
   },
 
-  data() {
-    return {
-      selectedFile: null,
-    };
-  },
   methods: {
     onFileSelected(event) {
       this.selectedFile = event.target.files[0];
     },
   },
+  data: function () {
+    return {
+      StianSulebak,
+    };
+  },
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.pic {
+  margin-bottom: 1em;
+  width: 100px;
+  height: 100px;
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+</style>
